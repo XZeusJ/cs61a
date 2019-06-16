aa25c2617889c01e81b554e79d637e381c5c14a5
XzJ <394386241@qq.com>
2019-06-16 14:14:27 +0800

    Recursion & Tree Recursion

--- lab&disc/disc03.py
+++ lab&disc/disc03.py
@@ -41,20 +41,52 @@ incr_1 = make_func_repeater(lambda x: x + 1, 1)
 
 # 2.1
 def count_stair_ways(n):
-	if n <= 2:
-		return n
-	return count_stair_ways(n-1)+count_stair_ways(n-2)
+	if n <= 3:
+		return 2**(n-1)
+	return count_stair_ways(n-1)+count_stair_ways(n-2)+count_stair_ways(n-3)
 # print(count_stair_ways(1))
 # print(count_stair_ways(2))
 # print(count_stair_ways(3))
-# print(count_stair_ways(4))
+# print(count_stair_ways(10))
 
 # 2.2
-result = 0
+# def count_k(n, k):
+# 	if n == 0:
+# 		return 1
+# 	elif n < 0:
+# 		return 0
+# 	else:
+# 		total = 0
+# 		for i in range(1, k+1):
+# 			total += count_k(n-i,k)
+# 		return total
+
 def count_k(n, k):
 	if n <= k:
-		return n
-	for i in range(1, k+1):
-		result += count_k(n-i)
-	return result
-print(count_k(4,4))
+		return 2**(n-1)
+
+	else:
+		total = 0
+		for i in range(1, k+1):
+			total += count_k(n-i,k)
+		return total
+
+# print(count_k(3,3))
+# print(count_k(4,4))
+# print(count_k(5,3))
+# print(count_k(300,1))
+
+#2.3
+def pascal(row, column):
+	if row < column: return 0
+	if column == 0: return 1
+	if row <= 1: return 1
+	return pascal(row-1, column-1) + pascal(row-1, column)
+
+# print(pascal(0,0))
+# print(pascal(1,0))
+# print(pascal(1,1))
+# print(pascal(2,0))
+# print(pascal(2,1))
+# print(pascal(2,2))
+# print(pascal(4,2))
