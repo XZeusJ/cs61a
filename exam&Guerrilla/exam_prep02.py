53543ed002796ee598c5fdcf04e29728b1a223d9
XzJ <394386241@qq.com>
2019-06-24 22:33:47 +0800

    update files

--- exam&Guerrilla/exam_prep02.py
+++ exam&Guerrilla/exam_prep02.py
@@ -1,2 +1,18 @@
 def print_numbers(n, k):
-	pass
+	def inner(n, s):
+		if n == 0:
+			if
+		else:
+
+
+	inner(n, 0)
+
+# define fn about reverse digit
+def reverse(n, s):
+	if n == 0:
+		return 0;
+	else:
+		return reverse();
+
+# num % 10 * 10**s + num / 10 (s=4)
+# 13597 -> num % 10 * 10* 10**s + num /10 (s=1) -> 97531
