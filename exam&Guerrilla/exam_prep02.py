0422d63445987d5478a8f0661a8dc0aca6d897f8
XzJ <394386241@qq.com>
2019-06-25 12:17:00 +0800

    update files

--- exam&Guerrilla/exam_prep02.py
+++ exam&Guerrilla/exam_prep02.py
@@ -1,18 +1,32 @@
-def print_numbers(n, k):
-	def inner(n, s):
-		if n == 0:
-			if
-		else:
+# def print_numbers(n, k):
+# 	def inner(n, s):
+# 		if n == 0:
+# 			if n // k == 0:
+# 				print(n);
+# 		else:
 
+# 	inner(n, 0)
 
-	inner(n, 0)
-
-# define fn about reverse digit
-def reverse(n, s):
+# define fn about reverse digit by recursion
+def reverse_rec(n,rmd = 0):
 	if n == 0:
-		return 0;
+		return rmd;
 	else:
-		return reverse();
+		return reverse(n // 10, 10 * rmd + (n%10));
+# print(reverse_rec(123))
 
-# num % 10 * 10**s + num / 10 (s=4)
-# 13597 -> num % 10 * 10* 10**s + num /10 (s=1) -> 97531
+# define fn about reverse digit by iteration
+def reverse_iter(n):
+	i = 0;
+	reverse = 0;
+	n = 123
+	while (n != 0):
+		print("n:",n)
+		reminder = n%10
+		reverse = (reverse*10)+reminder
+		print("reverse:",reverse)
+		n //= 10;
+# print(reverse_iter)
+
+def generate_any_digits(n):
+	pass
