44c8736c68fb38fb0d48ca913367c17a048fc90f
XzJ <394386241@qq.com>
2019-08-13 23:48:00 +0800

    update files

--- project/ants/ants.py
+++ project/ants/ants.py
@@ -44,7 +44,13 @@ class Place(object):
                 self.ant = insect
             else:
                 # BEGIN Problem 9
-                assert self.ant is None, 'Two ants in {0}'.format(self)
+                # assert self.ant is None, 'Two ants in {0}'.format(self)
+                if self.ant.can_contain(insect):
+                    self.ant.contain_ant(insect)
+                if insect.can_contain(self.ant):
+                    insect.contain_ant(self.ant)
+                    self.ant = insect
+                assert not self.ant.can_contain(insect) or not insect.can_contain(self.ant), "what the fuck"
                 # END Problem 9
         else:
             self.bees.append(insect)
@@ -177,6 +183,7 @@ class Ant(Insect):
 
     # ADD CLASS ATTRIBUTES HERE
     blocks_path = True
+    is_container = False
 
     def __init__(self, armor=1):
         """Create an Ant with an ARMOR quantity."""
@@ -378,8 +385,9 @@ class BodyguardAnt(Ant):
     name = 'Bodyguard'
     # OVERRIDE CLASS ATTRIBUTES HERE
     # BEGIN Problem 9
-    implemented = False  # Change to True to view in the GUI
-
+    implemented = True  # Change to True to view in the GUI
+    food_cost = 4
+    is_container = True
     # END Problem 9
 
     def __init__(self, armor=2):
@@ -389,16 +397,20 @@ class BodyguardAnt(Ant):
     def can_contain(self, other):
         # BEGIN Problem 9
         "*** YOUR CODE HERE ***"
+        return self.contain_ant is None and other.is_container is False
         # END Problem 9
 
     def contain_ant(self, ant):
         # BEGIN Problem 9
         "*** YOUR CODE HERE ***"
+        self.contained_ant = ant
         # END Problem 9
 
     def action(self, colony):
         # BEGIN Problem 9
         "*** YOUR CODE HERE ***"
+        if self.contained_ant:
+            self.contained_ant.anction(colony);
         # END Problem 9
 
 
